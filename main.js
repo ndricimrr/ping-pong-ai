51aadb907bfe2346564d251357de4ad389f144a3
ndricimrr <ndricimrr@gmail.com>
2023-12-17 02:05:33 +0100

    Add TODO

--- main.js
+++ main.js
@@ -34,4 +34,5 @@ dispatchPing({ message: "Ping" });
 dispatchPong({ message: "Pong" });
 
 // TODO: Later on Consider Ping Pong between Iframes. With Post Messages.
+// TODO: Later on Consider throwing a ball in a grid of Iframes, and it bounces as if it were in a full large rectangle/circle using postMessage API if possible
 // TODO: Later on Consider Simple Ping pong between WebComponents with Communication BUS
