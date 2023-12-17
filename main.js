e7def4947e04e514938c5d235035c4bd301e9a9b
ndricimrr <ndricimrr@gmail.com>
2023-12-17 01:41:32 +0100

    Simple Ping Pong with custom events

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ function pingLoop() {
 }
 
 // Continuously print Ping Pong on the console cuz its cool :)
-setInterval(() => {
-  pingLoop();
-}, 2000);
+// setInterval(() => {
+//   pingLoop();
+// }, 2000);
+
+window.addEventListener("ping", (event) => {
+  console.log("...Ping received :", event.detail.message);
+});
+
+window.addEventListener("pong", (event) => {
+  console.log("...Pong received :", event.detail.message);
+});
+
+function dispatchPing(data) {
+  console.log("Sending Ping...");
+  const customEvent = new CustomEvent("ping", { detail: data });
+  window.dispatchEvent(customEvent);
+}
+
+function dispatchPong(data) {
+  console.log("Sending Pong...");
+  const customEvent = new CustomEvent("pong", { detail: data });
+  window.dispatchEvent(customEvent);
+}
+
+dispatchPing({ message: "Ping" });
+dispatchPong({ message: "Pong" });
