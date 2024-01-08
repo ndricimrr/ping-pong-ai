4b57d4d9dbac1a7047cd925501be44cdbd460807
ndricimrr <ndricimrr@gmail.com>
2024-01-08 23:59:08 +0100

    Add ball setters

--- ball.js
+++ ball.js
@@ -1,9 +1,30 @@
 class Ball {
   width;
   height;
+  element;
 
-  constructor(width, height) {
+  constructor(width, height, domId) {
     this.width = width;
     this.height = height;
+    this.domId = domId;
+    this.element = document.getElementById(domId);
+  }
+
+  /**
+   * Set width of ball
+   * @param {string} width in pixels
+   */
+  setWidth(width) {
+    this.width = width;
+    this.element.style.width = width;
+  }
+
+  /**
+   * Height of the ball
+   * @param {string} height in pixels/%/other
+   */
+  setHeight(height) {
+    this.height = height;
+    this.element.style.height = width;
   }
 }
